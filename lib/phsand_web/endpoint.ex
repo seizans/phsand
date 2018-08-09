39e9aada39e80264ef991d2f044b149ed280b4a4
Seizan Shimazaki <seizan.shimazaki@aktsk.jp>
2018-08-10 00:58:21 +0900

    不要な Plug を削除する

--- lib/phsand_web/endpoint.ex
+++ lib/phsand_web/endpoint.ex
@@ -1,14 +1,6 @@
 defmodule PhsandWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :phsand
 
-  # Serve at "/" the static files from "priv/static" directory.
-  #
-  # You should set gzip to true if you are running phoenix.digest
-  # when deploying your static files in production.
-  plug Plug.Static,
-    at: "/", from: :phsand, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
-
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
@@ -22,16 +14,7 @@ defmodule PhsandWeb.Endpoint do
     pass: ["*/*"],
     json_decoder: Poison
 
-  plug Plug.MethodOverride
   plug Plug.Head
-
-  # The session will be stored in the cookie and signed,
-  # this means its contents can be read but not tampered with.
-  # Set :encryption_salt if you would also like to encrypt it.
-  plug Plug.Session,
-    store: :cookie,
-    key: "_phsand_key",
-    signing_salt: "N19R8xwU"
 
   plug PhsandWeb.Router
 
